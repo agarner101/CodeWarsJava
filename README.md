65d65726667f035b58e1dc5e4e3fa2db224cabb7
Andrew Garner <andrewgarner101@gmail.com>
2018-04-03 09:42:48 -0700

    Freddy Types Updated doc

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Code Wars Java
 Code Wars attempts and solutions
-#### Total Solved: 4
+#### Total Solved: 5
 
 ## History
 ##### [Exes and Ohs](https://www.codewars.com/kata/55908aad6620c066bc00002a)
 ##### [Phone Number](http://www.codewars.com/kata/525f50e3b73515a6db000b83/train/java)
 ##### [Credit Card Masker](http://www.codewars.com/kata/5412509bd436bd33920011bc/train/java)
 ##### [Digital Root](https://www.codewars.com/kata/541c8630095125aba6000c00/train/java)
+##### [Freddy](https://www.codewars.com/kata/fat-fingers/train/java)
--- src/freddy_types/Freddy.java
+++ src/freddy_types/Freddy.java
@@ -2,14 +2,14 @@ package freddy_types;
 
 /**
  * Freddy
- *
+ * <p>
  * Freddy has a really fat left pinky finger, and every time Freddy tries to type an A,
  * he accidentally hits the CapsLock key!
-
- Given a string that Freddy wants to type, emulate the keyboard misses where each A supposedly pressed is replaced
- with CapsLock, and return the string that Freddy actually types. It doesn't matter if the A in the string is
- capitalized or not. When CapsLock is enabled, capitalization is reversed, but punctuation is not affected.
- *
+ * <p>
+ * Given a string that Freddy wants to type, emulate the keyboard misses where each A supposedly pressed is replaced
+ * with CapsLock, and return the string that Freddy actually types. It doesn't matter if the A in the string is
+ * capitalized or not. When CapsLock is enabled, capitalization is reversed, but punctuation is not affected.
+ * <p>
  * https://www.codewars.com/kata/fat-fingers/train/java
  * Created by andrewgarner on 4/3/18.
  */
@@ -27,18 +27,16 @@ class Freddy {
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             if (c == 'a' || c == 'A') {
-                caps = !caps;
-                continue;
+                caps = !caps; //Flip caps state
+                continue; //Don't add a's to string
             }
             if (caps) {
                 if (c >= 'A' && c <= 'Z') {
-                    c += 32;
+                    c += 32; //Adding 32 converts to lowercase
                 } else if (c >= 'a' && c <= 'z') {
-                    c -= 32;
+                    c -= 32; //Subtracting 32 converts to uppercase
                 }
             }
-
-
             sb.append(c);
         }
         return sb.toString();
